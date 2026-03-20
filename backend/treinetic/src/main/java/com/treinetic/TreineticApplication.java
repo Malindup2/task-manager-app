cdd26b41b191e753a681f254e38166e1864718df
malindup2 <malindupabasara22@gmail.com>
2026-03-20 12:03:47 +0530

    chore: initial project structure with backend and frontend scaffolding

---
+++ backend/treinetic/src/main/java/com/treinetic/TreineticApplication.java
@@ -0,0 +1,13 @@
+package com.treinetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreineticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreineticApplication.class, args);
+    }
+
+}
